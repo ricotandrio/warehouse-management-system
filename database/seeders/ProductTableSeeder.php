3ebbec19072140195d0de8fd431aa4020462ef9e
Rico Tandrio <119276763+ricotandrio@users.noreply.github.com>
2024-10-16 21:06:33 +0700

    feat: update seeder with faker (#4)

--- database/seeders/ProductTableSeeder.php
+++ database/seeders/ProductTableSeeder.php
@@ -2,47 +2,161 @@
 
 namespace Database\Seeders;
 
-use App\Models\Product;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class ProductTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        Product::create([
-            'name' => 'Samsung Galaxy S21+',
-            'price' => 14999999,
-            'sku' => 'SAMSUNG-GALAXY-S21-PLUS',
-            'stock_quantity' => 10,
-            'image' => 'https://cdn.dxomark.com/wp-content/uploads/medias/post-75482/SamsungGalaxys215gplus.jpg',
-            'manufacturer_id' => '14575a29-e374-4b3f-8e9e-fef18ba50390',
-            'category_id' => '80632c60-f132-4503-b69c-663e5ebd7481',
-        ]);
+        $uuidManager = UuidManager::getInstance();
+        $faker = Faker::create();
 
-        Product::create([
-            'name' => 'Apple iPhone 12 Pro Max',
-            'price' => 19999999,
-            'sku' => 'APPLE-IPHONE-12-PRO-MAX',
-            'stock_quantity' => 10,
-            'image' => 'https://images.tokopedia.net/img/cache/500-square/VqbcmM/2021/7/29/6f371226-0db7-4278-8f9d-9871e9bcfc27.jpg',
-            'manufacturer_id' => '14575a29-e374-4b3f-8e9e-fef18ba50390',
-            'category_id' => '80632c60-f132-4503-b69c-663e5ebd7481',
-        ]);
-
-        Product::create([
-            'name' => 'Xiaomi Redmi Note 10 Pro',
-            'price' => 4999999,
-            'sku' => 'XIAOMI-REDMI-NOTE-10-PRO',
-            'stock_quantity' => 10,
-            'image' => 'https://images.tokopedia.net/img/cache/700/VqbcmM/2021/5/4/f1bc70ed-7468-4d90-abd2-c4c064fbd22c.jpg',
-            'manufacturer_id' => '14575a29-e374-4b3f-8e9e-fef18ba50390',
-            'category_id' => '80632c60-f132-4503-b69c-663e5ebd7481',
+        DB::table('products')->insert([
+            [
+                'id' => $uuidManager->generate('samsung-galaxy-s21'),
+                'name' => 'Samsung Galaxy S21',
+                'price' => 12999999,
+                'sku' => 'SAMSUNG-GALAXY-S21',
+                'description' => $faker->paragraph,
+                'stock_quantity' => 10,
+                'image' => '/default/product.png',
+                'manufacturer_id' => $uuidManager->get('samsung'),
+                'category_id' => $uuidManager->get('smartphone'),
+                'created_at' => now(),
+                'created_by' => $uuidManager->get('admin'),
+            ],
+            [
+                'id' => $uuidManager->generate('apple-iphone-12-pro-max'),
+                'name' => 'Apple iPhone 12 Pro Max',
+                'price' => 19999999,
+                'sku' => 'APPLE-IPHONE-12-PRO-MAX',
+                'description' => $faker->paragraph,
+                'stock_quantity' => 10,
+                'image' => '/default/product.png',
+                'manufacturer_id' => $uuidManager->get('apple'),
+                'category_id' => $uuidManager->get('smartphone'),
+                'created_at' => now(),
+                'created_by' => $uuidManager->get('admin'),
+            ],
+            [
+                'id' => $uuidManager->generate('xiaomi-redmi-note-10-pro'),
+                'name' => 'Xiaomi Redmi Note 10 Pro',
+                'price' => 4999999,
+                'sku' => 'XIAOMI-REDMI-NOTE-10-PRO',
+                'description' => $faker->paragraph,
+                'stock_quantity' => 10,
+                'image' => '/default/product.png',
+                'manufacturer_id' => $uuidManager->get('xiaomi'),
+                'category_id' => $uuidManager->get('smartphone'),
+                'created_at' => now(),
+                'created_by' => $uuidManager->get('admin'),
+            ],
+            [
+                'id' => $uuidManager->generate('samsung-galaxy-tab-s7'),
+                'name' => 'Samsung Galaxy Tab S7',
+                'price' => 12999999,
+                'sku' => 'SAMSUNG-GALAXY-TAB-S7',
+                'description' => $faker->paragraph,
+                'stock_quantity' => 10,
+                'image' => '/default/product.png',
+                'manufacturer_id' => $uuidManager->get('samsung'),
+                'category_id' => $uuidManager->get('tablet'),
+                'created_at' => now(),
+                'created_by' => $uuidManager->get('admin'),
+            ],
+            [
+                'id' => $uuidManager->generate('apple-ipad-pro-2021'),
+                'name' => 'Apple iPad Pro 2021',
+                'price' => 19999999,
+                'sku' => 'APPLE-IPAD-PRO-2021',
+                'description' => $faker->paragraph,
+                'stock_quantity' => 10,
+                'image' => '/default/product.png',
+                'manufacturer_id' => $uuidManager->get('apple'),
+                'category_id' => $uuidManager->get('tablet'),
+                'created_at' => now(),
+                'created_by' => $uuidManager->get('admin'),
+            ],
+            [
+                'id' => $uuidManager->generate('xiaomi-mi-pad-5'),
+                'name' => 'Xiaomi Mi Pad 5',
+                'price' => 4999999,
+                'sku' => 'XIAOMI-MI-PAD-5',
+                'description' => $faker->paragraph,
+                'stock_quantity' => 10,
+                'image' => '/default/product.png',
+                'manufacturer_id' => $uuidManager->get('xiaomi'),
+                'category_id' => $uuidManager->get('tablet'),
+                'created_at' => now(),
+                'created_by' => $uuidManager->get('admin'),
+            ],
+            [
+                'id' => $uuidManager->generate('samsung-galaxy-book-s'),
+                'name' => 'Samsung Galaxy Book S',
+                'price' => 12999999,
+                'sku' => 'SAMSUNG-GALAXY-BOOK-S',
+                'description' => $faker->paragraph,
+                'stock_quantity' => 10,
+                'image' => '/default/product.png',
+                'manufacturer_id' => $uuidManager->get('samsung'),
+                'category_id' => $uuidManager->get('laptop'),
+                'created_at' => now(),
+                'created_by' => $uuidManager->get('admin'),
+            ],
+            [
+                'id' => $uuidManager->generate('apple-macbook-air-2021'),
+                'name' => 'Apple MacBook Air 2021',
+                'price' => 19999999,
+                'sku' => 'APPLE-MACBOOK-AIR-2021',
+                'description' => $faker->paragraph,
+                'stock_quantity' => 10,
+                'image' => '/default/product.png',
+                'manufacturer_id' => $uuidManager->get('apple'),
+                'category_id' => $uuidManager->get('laptop'),
+                'created_at' => now(),
+                'created_by' => $uuidManager->get('admin'),
+            ],
+            [
+                'id' => $uuidManager->generate('xiaomi-redmi-book-pro'),
+                'name' => 'Xiaomi Redmi Book Pro',
+                'price' => 4999999,
+                'sku' => 'XIAOMI-REDMI-BOOK-PRO',
+                'description' => $faker->paragraph,
+                'stock_quantity' => 10,
+                'image' => '/default/product.png',
+                'manufacturer_id' => $uuidManager->get('xiaomi'),
+                'category_id' => $uuidManager->get('laptop'),
+                'created_at' => now(),
+                'created_by' => $uuidManager->get('admin'),
+            ],
+            [
+                'id' => $uuidManager->generate('samsung-odyssey-g9'),
+                'name' => 'Samsung Odyssey G9',
+                'price' => 12999999,
+                'sku' => 'SAMSUNG-ODYSSEY-G9',
+                'description' => $faker->paragraph,
+                'stock_quantity' => 10,
+                'image' => '/default/product.png',
+                'manufacturer_id' => $uuidManager->get('samsung'),
+                'category_id' => $uuidManager->get('desktop'),
+                'created_at' => now(),
+                'created_by' => $uuidManager->get('admin'),
+            ],
+            [
+                'id' => $uuidManager->generate('apple-imac-2021'),
+                'name' => 'Apple iMac 2021',
+                'price' => 19999999,
+                'sku' => 'APPLE-IMAC-2021',
+                'description' => $faker->paragraph,
+                'stock_quantity' => 10,
+                'image' => '/default/product.png',
+                'manufacturer_id' => $uuidManager->get('apple'),
+                'category_id' => $uuidManager->get('desktop'),
+                'created_at' => now(),
+                'created_by' => $uuidManager->get('admin'),
+            ],
         ]);
     }
 }
